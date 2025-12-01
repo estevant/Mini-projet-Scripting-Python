ebf85db88eff83e997ef11a06d68fce219ea99ce
Evan STEVANT <stevantevan04@gmail.com>
2025-12-01 13:35:48 +0100

    wip
    fichier pour ajouter direct le superadmin dans la bdd
    correction de la suppression avec le login maintenant

--- Projet_gestion_des_utilisateurs/bdd.sql
+++ Projet_gestion_des_utilisateurs/bdd.sql
@@ -1,14 +1,11 @@
 CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50), 
-    prenom VARCHAR(50), 
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
     mail VARCHAR(100),
     login VARCHAR(50) UNIQUE,
     password VARCHAR(255),
     role VARCHAR(20) DEFAULT 'user'
 );
-
-INSERT INTO users (nom, prenom, mail, login, password, role) 
-VALUES ('Super', 'Admin', 'admin.paris@hopital.com', 'superadmin', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2', 'superadmin');
 
 select * from users; pour voir la table
--- Projet_gestion_des_utilisateurs/suppression_utilisateur.py
+++ Projet_gestion_des_utilisateurs/suppression_utilisateur.py
@@ -8,11 +8,11 @@ def delete_user():
     cursor = connexion.cursor()
     print("connexion établie")
     # print("Veillez saisir les info liee a l utilisateur que vous souhaitez supprimer")
-    supp_nom=input("saisir le nom de l'utilisateur : ")
+    supp_login=input("saisir le login de l'utilisateur : ")
     #supp_prenom=input("saisir le prenom de l'utilisateur : ")
     # supp_mail=input("saisir le mail de l'utilisateur : ")
     # user_delete=(supp_nom,supp_prenom,supp_mail)
-    cursor.execute("DELETE FROM users WHERE nom = %s",(supp_nom,))
+    cursor.execute("DELETE FROM users WHERE login = %s",(supp_login,))
 
     connexion.commit()
     print("l utilisateur a ete supprime")
